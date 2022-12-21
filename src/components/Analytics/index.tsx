23ec51ac48ce5a95e06ac943172f927daa0c13b3
Marcelo Henrique <marcelo@siasp.com.br>
2022-12-21 18:07:18 -0300

    Adicionando Analyttics

---
+++ src/components/Analytics/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Analytics = () => (
+  <>
+    <script 
+      async 
+      src={`{https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING}}`}
+    />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING}', {
+            page_path: window.location.pathname,
+          });
+        `
+      }}
+    />
+  </>
+)
+// gtag('config', 'G-N8S1RF1C18');
+export default Analytics;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css'
 import { DefaultSeo } from 'next-seo';
 import SEO from '../../next-seo-config';
 import type { AppProps } from 'next/app'
+import Analytics from '../components/Analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
