80af18937818d2adb8b391cf829939690e0f52e0
Marcelo Henrique <marcelo@siasp.com.br>
2022-12-22 09:47:57 -0300

    Correção para Analytics

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -1,24 +1,28 @@
 import React from "react";
+import { useRouter } from "next/router";
 
-const Analytics = () => (
-  <>
-    <script 
-      async 
-      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING}`}
-    />
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING}', {
-            page_path: '${window.location.pathname}',
-          });
-        `
-      }}
-    />
-  </>
-)
+export default function Analytics () {
+  const routes = useRouter();
+
+  return (
+    <>
+      <script 
+        async 
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING}', {
+              page_path: '${routes.asPath}',
+            });
+          `
+        }}
+      />
+    </>
+  )
+}
 // gtag('config', 'G-N8S1RF1C18');
-export default Analytics;
